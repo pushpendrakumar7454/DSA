f076c4e59e82616de5f1294fe7eca6f3297540e2
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-06-27 08:33:58 +0530

    string programs

--- string/palindrome.js
+++ string/palindrome.js
@@ -1,11 +1,21 @@
 const promt=require('prompt-sync')()
 
 let str=promt("Enter your string-::")
-let rev="";
 
-for(let i=0;i<str.length;i++){
-    rev=str[i]+rev
+let i=0;
+let j=str.length-1
+let isPalindrome=true
+
+while(i<j){
+    if(str.charAt[i]!=str.charAt[j]){
+        isPalindrome=false
+        break
+    }
+    j--
+    i++
 }
-if(rev==str) console.log("Palindrome");
+if(isPalindrome) console.log("Palindrome");
 else console.log("Not Palindrome");
+
+
 
