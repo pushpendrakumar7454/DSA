455f0a3a7c2b2bb9822ae0324a4f8d1330e31bf2
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-04 11:04:37 +0530

    firsttwiceletter

---
+++ Set/firstlettertwice.js
@@ -0,0 +1,31 @@
+let str = "abccbaacz";
+
+
+let set = new Set();
+
+for (let i = 0; i < str.length; i++) {
+    if (set.has(str[i])) {
+        console.log(str[i]);
+        break;
+    }
+
+    set.add(str[i]);
+}
+
+
+
+
+// for(let i=0;i<str.length;i++){
+//     let count=0;
+//     for(let j=0;j<=i;j++){
+//         if(str[i]==str[j]){
+//             count++
+
+//         }
+//     }
+//     if(count==2){
+//         console.log(str[i]);
+//        break
+        
+//     }
+// }
