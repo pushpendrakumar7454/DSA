8a4b96d79f25c11971bc4466a668d200a1f72f27
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-07 09:42:03 +0530

    happy number

--- Set/happyNumber.js
+++ Set/happyNumber.js
@@ -3,4 +3,18 @@ let promt=require('prompt-sync')()
 let num=Number(promt("Enter your number-::"))
 let result=num;
 
-while()
+while(result!==1 && result!==4){
+    let sum=0;
+
+    while(result>0){
+        let dig=result%10
+        sum=sum+(dig*dig)
+        result=Math.floor(result/10)
+    }
+   result=sum;
+}
+
+if(result===1) console.log(num," is Happy Number");
+
+else console.log(num, " Is not Happy Number");
+
