447288e17f53212edd9cfbc0f2e263a3eadd8dad
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-08 08:46:38 +0530

    twosum

--- Map/twosum.js
+++ Map/twosum.js
@@ -1,19 +1,35 @@
-let arr=[2,3,4,5,6,7]
-let target=11
+let arr=[2,3,4,30,6,7]
+let target=34
 
-let found =false;
+
+let ans=[]
+let map=new Map()
 
 for(let i=0;i<arr.length;i++){
-    for(let j=i+1;j<arr.length;j++){
-        if(arr[i]+arr[j]===target){
-            found=true
-            console.log(i,j);
-            break
-            
-        }
+    if(map.has(target-arr[i])){
+        ans.push(i)
+        ans.push(map.get(target-arr[i]))
+    }else{
+        map.set(arr[i],i)
     }
 }
 
-if(found) console.log("Two Sum");
-else console.log("Not Two sum");
+console.log(ans);
+
+
+// let found =false;
+
+// for(let i=0;i<arr.length;i++){
+//     for(let j=i+1;j<arr.length;j++){
+//         if(arr[i]+arr[j]===target){
+//             found=true
+//             console.log(i,j);
+//             break
+            
+//         }
+//     }
+// }
+
+// if(found) console.log("Two Sum");
+// else console.log("Not Two sum");
 
