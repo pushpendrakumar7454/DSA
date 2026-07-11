0b7f69a6e936ce95f053f5a0369dab714faf16fc
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-11 08:41:53 +0530

    factorial

---
+++ recursion/fac.js
@@ -0,0 +1,10 @@
+let prompt = require("prompt-sync")();
+
+let num = Number(prompt("Enter your num-::"));
+
+function factorial(num){
+    if(num==1) return 1;
+    return num*factorial(num-1)
+}
+
+console.log(factorial(num));
