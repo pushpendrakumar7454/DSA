5edae48ec8607c817374b28a0c929e9e33a99d15
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-14 08:20:32 +0530

    Count prime

--- Mathproblems/countPrime.js
+++ Mathproblems/countPrime.js
@@ -4,9 +4,10 @@ let num = Number(prompt("Enter your num-::"));
 
 let count=0;
 
-for(let i=2;i<=num;i++){
+for(let i=2;i<num;i++){
     isPrirme=true;
-    for(let j=2;j<=Math.sqrt(i);j++){
+    // for(let j=2;j<=Math.sqrt(i);j++)
+    for(let j=2;j*j<=(i);j++){
         if(i%j==0){
             isPrirme=false;
             break
