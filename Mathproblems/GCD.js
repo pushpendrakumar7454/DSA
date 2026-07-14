1d7e60eb54f3b2a3d5103204b95457c3166c392a
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-14 07:23:05 +0530

    GCD

--- Mathproblems/GCD.js
+++ Mathproblems/GCD.js
@@ -1,17 +1,28 @@
 let prompt = require("prompt-sync")();
 
 let a = Number(prompt("Enter first number: "));
-let b=Number(prompt("ENter second number-::"))
+let b=Number(prompt("Enter second number-::"))
 
 
 //Brute Force Approach
-let ans=0;
-let min=Math.min(a,b)
-for(let i=min;i>=1;i--){
-    if(a%i==0 && b%i==0){
-        ans=i;
-        break
-    }
+// let ans=0;
+// let min=Math.min(a,b)
+// for(let i=min;i>=1;i--){
+//     if(a%i==0 && b%i==0){
+//         ans=i;
+//         break
+//     }
+// }
+
+// console.log(ans);
+
+
+
+// better approach
+while(a!=b){
+    if(a>b) a=a-b
+    else b=b-a
 }
 
-console.log(ans);
+console.log(b);
+
