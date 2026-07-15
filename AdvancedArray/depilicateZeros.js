5a8ea76dc71bafc8bad0968e2a2341b83e7e960f
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-15 08:01:25 +0530

    duplicate Zeros

---
+++ AdvancedArray/depilicateZeros.js
@@ -0,0 +1,27 @@
+let arr=[3,4,0,4,5,0,32,0]
+
+let count=0;
+let n=arr.length
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]===0){
+        count++
+    }
+}
+let i=n-1,j=count+n-1
+
+while(i>=0){
+    if(j<n){
+        arr[j]=arr[i]
+    }
+    if(arr[i]==0){
+        j--
+        if(j<n){
+            arr[j]=0
+        }
+    }
+    j--
+    i--
+}
+
+console.log(arr);
