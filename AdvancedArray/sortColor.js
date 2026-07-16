7b6a8f7556f49a1bb866cd3db1302c382ddeccbb
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-16 07:28:51 +0530

    sortColor

---
+++ AdvancedArray/sortColor.js
@@ -0,0 +1,30 @@
+// Dutch National Flag Algorithm
+let arr=[2,0,1,1,2,0,1,1]
+let l=arr.length
+
+function swap(arr,i,j){
+    let temp=arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+}
+
+
+let i=0,j=0,k=l-1;
+
+while(i<=k){
+    if(arr[i]==0){
+        swap(arr,i,j)
+        i++;
+        j++
+    }else if(arr[i]===2){
+        swap(arr,i,k)
+        k--
+    }else{
+        i++
+    }
+}
+
+
+console.log(arr);
+
+
