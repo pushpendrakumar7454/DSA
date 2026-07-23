a5bf1f76cee541a972f6d74056e1a44e89fbf8ec
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-23 08:17:34 +0530

    merge sort

--- sortingAlgorithm/mergeSort.js
+++ sortingAlgorithm/mergeSort.js
@@ -1,29 +1,15 @@
 
-function merge(arr,f,l,mid){
-    let temp=new Array(l-f+1)
+
+function merge(f,l,mid,arr){
+    let temp=new Array(l-f+1);
     let i=f,j=mid+1,k=0;
     while(i<=mid && j<=l){
         if(arr[i]<arr[j]) temp[k++]=arr[i++]
-        else temp[k++] =arr[j++]
+        else temp[k++]=arr[j++]
     }
-    while(i<=mid) temp[k++]=arr[i++]
-    while(j<=l) temp[k++]=arr[j++]
 
-    i=f,k=0;
+    while(i<=mid) temp[k++]=arr[i++]
+    while(j<=l) temp[k++]=arr[i++]
+    i=k,k=0
     while(k<temp.length) arr[i++]=temp[k++]
 }
-
-
-
-function divide(arr,f,l){
-    if(f>=l) return 
-    let mid=Math.floor((f+l)/2)
-    divide(arr,f,mid)
-    divide(arr,mid+1,l)
-    merge(arr,f,l,mid)
-}
-
-
-let arr=[98,2,34,2,1,345,65,7,34]
-divide(arr,0,arr.length-1)
-console.log(arr);
