9ecc6cd484e92f3b82f2d87fbbb3a1d643a82196
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-23 08:59:56 +0530

    MergeSort

--- sortingAlgorithm/mergeSort.js
+++ sortingAlgorithm/mergeSort.js
@@ -1,14 +1,14 @@
 function merge(arr,f,l,mid){
     let temp=new Array(l-f+1)
     let i=f,j=mid+1,k=0;
+
     while(i<=mid && j<=l){
         if(arr[i]<arr[j]) temp[k++]=arr[i++]
         else temp[k++]=arr[j++]
     }
     while(i<=mid) temp[k++]=arr[i++]
-    while(j<=l) temp[k++]=arr[i++]
+    while(j<=l) temp[k++]=arr[j++]
     i=f,k=0
-
     while(k<temp.length) arr[i++]=temp[k++]
 }
 
@@ -20,7 +20,6 @@ function devide(arr,f,l){
     merge(arr,f,l,mid)
 }
 
-
-let arr=[54,23,34,5,5]
+let arr=[44,2,23,4,56,5]
 devide(arr,0,arr.length-1)
 console.log(arr);
