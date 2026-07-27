b3b393cea2810b2c363dfa27c7307c376c2cb6a0
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-27 07:03:52 +0530

    sort

--- sortingAlgorithm/quickSort.js
+++ sortingAlgorithm/quickSort.js
@@ -36,3 +36,50 @@
 
 
 
+function findpartitions(arr,f,l){
+    let i=f-1,j=f,pivot=arr[l]
+    while(j<l){
+        if(arr[j]<pivot){
+            i++
+            swap(arr,i,j)
+        }
+        j++
+    }
+    i++
+    swap(arr,i,l)
+    return i
+}
+
+function swap(arr,i,j){
+    let temp=arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+}
+
+
+function quickSort(arr,f,l){
+    if(f>l) return 
+    let pInx=findpartitions(arr,f,l)
+    quickSort(arr,f,pInx-1)
+    quickSort(arr,pInx+1,l)
+}
+
+let arr=[33,43,2,1,4,6,23]
+quickSort(arr,0,arr.length-1)
+console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
