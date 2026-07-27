fb7ab3fc5643ee2d9cbf277cd092951e118eec15
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-27 06:53:45 +0530

    sort

--- sortingAlgorithm/quickSort.js
+++ sortingAlgorithm/quickSort.js
@@ -1,34 +1,38 @@
-function findpartitions(arr,f,l){
-    let i=f-1,j=f,pivot=arr[l]
-    while(j<l){
-        if(arr[j]<pivot){
-            i++
-            swap(arr,i,j)
-        }
-        j++
-    }
-    i++
-    swap(arr,i,l)
-    return i
+// function findpartitions(arr,f,l){
+//     let i=f-1,j=f,pivot=arr[l]
+//     while(j<l){
+//         if(arr[j]<pivot){
+//             i++
+//             swap(arr,i,j)
+//         }
+//         j++
+//     }
+//     i++
+//     swap(arr,i,l)
+//     return i
 
-}
+// }
 
-function swap(arr,i,j){
-    let temp=arr[i]
-    arr[i]=arr[j]
-    arr[j]=temp
-}
+// function swap(arr,i,j){
+//     let temp=arr[i]
+//     arr[i]=arr[j]
+//     arr[j]=temp
+// }
 
 
-function quickSort(arr,f,l){
-    if(f>l) return
-    let pInx=findpartitions(arr,f,l)
-    quickSort(arr,f,pInx-1)
-    quickSort(arr,pInx+1,l)
+// function quickSort(arr,f,l){
+//     if(f>l) return
+//     let pInx=findpartitions(arr,f,l)
+//     quickSort(arr,f,pInx-1)
+//     quickSort(arr,pInx+1,l)
 
-}
+// }
 
-let arr=[44,3,2,3,56,4,6,7]
-quickSort(arr,0,arr.length-1)
-console.log(arr);
+// let arr=[44,3,2,3,56,4,6,7]
+// quickSort(arr,0,arr.length-1)
+// console.log(arr);
+
+
+
+
 
