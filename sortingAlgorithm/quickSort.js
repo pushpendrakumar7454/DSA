fb68223b504641e864c00c4fbcabbaa7663da80f
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-27 07:16:35 +0530

    quickSort

--- sortingAlgorithm/quickSort.js
+++ sortingAlgorithm/quickSort.js
@@ -1,43 +1,3 @@
-// function findpartitions(arr,f,l){
-//     let i=f-1,j=f,pivot=arr[l]
-//     while(j<l){
-//         if(arr[j]<pivot){
-//             i++
-//             swap(arr,i,j)
-//         }
-//         j++
-//     }
-//     i++
-//     swap(arr,i,l)
-//     return i
-
-// }
-
-// function swap(arr,i,j){
-//     let temp=arr[i]
-//     arr[i]=arr[j]
-//     arr[j]=temp
-// }
-
-
-// function quickSort(arr,f,l){
-//     if(f>l) return
-//     let pInx=findpartitions(arr,f,l)
-//     quickSort(arr,f,pInx-1)
-//     quickSort(arr,pInx+1,l)
-
-// }
-
-// let arr=[44,3,2,3,56,4,6,7]
-// quickSort(arr,0,arr.length-1)
-// console.log(arr);
-
-
-
-
-
-
-
 function findpartitions(arr,f,l){
     let i=f-1,j=f,pivot=arr[l]
     while(j<l){
@@ -53,20 +13,28 @@ function findpartitions(arr,f,l){
 
 }
 
-
 function swap(arr,i,j){
     let temp=arr[i]
     arr[i]=arr[j]
     arr[j]=temp
 }
 
+
 function quickSort(arr,f,l){
     if(f>l) return
-    let pIdx=findpartitions(arr,f,l)
-    quickSort(arr,f,pIdx-1)
-    quickSort(arr,pIdx+1,l)
+    let pInx=findpartitions(arr,f,l)
+    quickSort(arr,f,pInx-1)
+    quickSort(arr,pInx+1,l)
+
 }
 
-let arr=[34,455,73,2,3,4,5]
+let arr=[44,3,2,3,56,4,6,7]
 quickSort(arr,0,arr.length-1)
 console.log(arr);
+
+
+
+
+
+
+
