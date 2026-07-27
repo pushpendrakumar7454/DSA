04ace5d451d2510b32c0d4088f1dc28674fe36af
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-27 08:20:51 +0530

    advanced binarySearch

--- SearchAlgorithm/AdvancedBinarySearch.js
+++ SearchAlgorithm/AdvancedBinarySearch.js
@@ -1,38 +1,55 @@
 let arr=[56,42,12,34,55,6]
 let st=0,l=arr.length-1
-let t=42;
+let t=54;
 let index=-1;
 
 
-function sort(){
-    for(let i=0;i<arr.length-1;i++){
-        for(let j=0;j<arr.length-1;j++){
-            if(arr[j+1]<arr[j]){
-                let temp=arr[j]
-                arr[j]=arr[j+1]
-                arr[j+1]=temp
-            }
+
+
+function findpartitions(arr,f,l){
+    let i=f-1,j=f,pivot=arr[l]
+    while(j<l){
+        if(arr[j]<pivot){
+            i++
+            swap(arr,i,j)
         }
+        j++
     }
-    return arr
+    i++
+    swap(arr,i,l)
+    return i
 }
-sort()
+
+function swap(arr,i,j){
+    let temp=arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+}
+
+function quickSort(arr,f,l){
+    if(f>l) return 
+    let cIdx=findpartitions(arr,f,l)
+    quickSort(arr,f,cIdx-1)
+    quickSort(arr,cIdx+1,l)
+}
+
+quickSort(arr,0,arr.length-1)
 console.log(arr);
+
 
 while(st<=l){
     let mid=Math.floor((st+l)/2)
-    if(arr[mid]==t){
-        index=mid
+    if(arr[mid]===t){
+        index=mid;
         break
-    }else if(arr[mid]<t) st=mid+1
+    }else if(arr[mid]<t) {
+        index=mid
+        st=mid+1
+    }
     else l=mid-1
 }
 
-if(index===-1){
-    console.log("Not found");
-    
+console.log(index);
 
-}else {
-    console.log("Founded");
-    
-}
+
+
