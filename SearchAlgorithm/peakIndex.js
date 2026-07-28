9a7e2102f9ba5713102ae80f69d348af17287415
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-28 07:42:46 +0530

    peak

--- SearchAlgorithm/peakIndex.js
+++ SearchAlgorithm/peakIndex.js
@@ -1,14 +1,36 @@
-let arr=[2,4,6,10,1,2]
+let arr = [2,3,4,5,10,2,1];
 
+let st = 0;
+let ed = arr.length - 1;
 
+while (st < ed) {
+    let mid = Math.floor((st + ed) / 2);
 
-for(let i=0;i<arr.length;i++){
-    let f=i-1,l=i+1
-    if(arr[i]>arr[f] && arr[i]>arr[l]){
-        console.log(arr[i]);
-        
+    if (arr[mid] < arr[mid + 1]) {
+        st = mid + 1;
+    } else {
+        ed = mid;
     }
-    
 }
+
+console.log(st); // 3
+console.log(arr[st]); // 10
+
+
+
+
+
+
+
+
+//brute forse
+// for(let i=0;i<arr.length;i++){
+//     let f=i-1,l=i+1
+//     if(arr[i]>arr[f] && arr[i]>arr[l]){
+//         console.log(arr[i]);
+        
+//     }
+    
+// }
 
 
