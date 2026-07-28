11bb9747d4f70bffac6eaa6a73dacc951e483bee
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-28 08:22:56 +0530

    Search in Rotated Sorted Array

--- SearchAlgorithm/SearchinRotatedSortedArray.js
+++ SearchAlgorithm/SearchinRotatedSortedArray.js
@@ -1,31 +1,24 @@
 let arr=[4,5,6,7,0,1,2]
 let t=0;
-let st=0;
-let ed=arr.length-1
+let st=0,ed=arr.length-1
 let ans=-1
 
 while(st<=ed){
     let mid=Math.floor((st+ed)/2)
-    if(arr[mid]==t){
+    if(arr[mid]===t){
         ans=mid
         break
     }
+    //left roated
     if(arr[st]<arr[mid]){
-        if(t>=arr[st] && t<arr[mid]){
+        if(t>=arr[st] &&t<arr[mid]){
             ed=mid-1
-
-
-        }else{
-            st=mid+1
-        }
+        }else st=mid+1
     }else{
-        if(t>arr[ed] && t<=arr[mid]){
-            st=mid+1
-        }else{
-            ed=mid-1
-        }
+        if(t<arr[ed] && t>=arr[mid]) st=mid+1
+        else ed=mid-1
     }
-}
 
+}
 
 console.log(ans);
