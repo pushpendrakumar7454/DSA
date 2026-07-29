03dd115c72762372df0e5eaef90b7a43a075e354
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-29 07:56:27 +0530

    Intersection Of Two Array.

--- Map/intersectionoftwoarray.js
+++ Map/intersectionoftwoarray.js
@@ -1,14 +1,19 @@
 let arr1 = [4, 5, 65, 4, 6];
 let arr2 = [10, 2, 3, 4, 67, 4];
 
-let map=new Map()
+let map = new Map();
 
-for(let i=0;i<arr1.length;i++){
-    if(map.has(arr1[i])){
-        map.set(arr1[i],map.get(arr1[i])+1)
-
-    }else{
-        map.set(arr1[i],1)
-    }
+for (let i = 0; i < arr1.length; i++) {
+  if (map.has(arr1[i])) {
+    map.set(arr1[i], map.get(arr1[i]) + 1);
+  } else {
+    map.set(arr1[i], 1);
+  }
 }
-console.log(map);
+
+for (let i = 0; i < arr2.length; i++) {
+  if (map.has(arr2[i]) && map.get(arr2[i]) > 0) {
+    console.log(arr2[i]);
+    map.has(arr2[i], map.get(arr2[i]), -1);
+  }
+}
