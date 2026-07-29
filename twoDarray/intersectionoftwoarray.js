11d58663be819b9faeecd6653f5b199f32cf763b
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-07-29 07:25:33 +0530

    Intersection Of Two Array

--- twoDarray/intersectionoftwoarray.js
+++ twoDarray/intersectionoftwoarray.js
@@ -1,12 +1,28 @@
-let arr1=[4,5,65,4,6]
-let arr2=[10,2,3,4,67]
+let arr1=[1,2,3,4,5]
+let arr2=[5,6,7,8,9]
 
-let ans=0
 
-for(let i=0;i<arr1.length;i++){
-    for(let j=0;j<arr2.length;j++){
-        if(arr1[i]===arr2[j]){
-            console.log(arr1[i]); 
-        }
+// //optimize approarch
+let i=0,j=0
+
+while(i<arr1.length && j<arr2.length){
+    if(arr1[i]===arr2[j]){
+        console.log(arr1[i]);
+        i++
+        j++
+        
+    }else if(arr1[i]<arr2[i]){
+        i++
+    }else{
+        j++
     }
 }
+
+// brute forse
+// for(let i=0;i<arr1.length;i++){
+//     for(let j=0;j<arr2.length;j++){
+//         if(arr1[i]===arr2[j]){
+//             console.log(arr1[i]); 
+//         }
+//     }
+// }
