8c6ef2ef35b29861b3a788e47c8c3ccaaa9ca2bc
natural7454 <pushpendrakumar7454981358@gmail.com>
2026-08-07 07:42:18 +0530

    insertAttFirst

--- LinkesList/linkedList.js
+++ LinkesList/linkedList.js
@@ -1,55 +1,22 @@
-class Node {
-    constructor(val) {
-        this.data = val;
-        this.next = null;
+class Node{
+    constructor(val){
+        this.data=val
+        this.next=null
     }
 }
 
-class LL {
-    constructor() {
-        this.head = null;
-        this.size = 0;
+
+class LL{
+    constructor(){
+        this.head=null
+        this.size=0
+
+       
     }
-
-    insertedFisrt(val) {
-        this.size++;
-
-        let newNode = new Node(val);
-
-        if (this.head === null) {
-            this.head = newNode;
-            return;
+    inserAtFirst(val){
+        let newNode=new Node(val)
+        if(this.head==null){
+             this.head=newNode
         }
-
-        newNode.next = this.head;
-        this.head = newNode;
-    }
-
-    printLL(head) {
-        if (head === null) {
-            console.log("Empty Linked List");
-            return;
-        }
-
-        let temp = head;
-        let ans = "";
-
-        while (temp !== null) {
-            ans += temp.data + " -> ";
-            temp = temp.next;
-        }
-
-        ans += "null";
-        console.log(ans);
     }
 }
-
-let obj = new LL();
-
-obj.insertedFisrt(89);
-obj.insertedFisrt(99);
-obj.insertedFisrt(19);
-obj.insertedFisrt(0);
-obj.insertedFisrt(9);
-
-obj.printLL(obj.head);
