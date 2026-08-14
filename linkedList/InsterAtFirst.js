5925a4a7523bfdf70228bd47aa65de89a2a7880a
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-14 08:16:30 +0530

    deletedByPosttion

--- linkedList/InsterAtFirst.js
+++ linkedList/InsterAtFirst.js
@@ -52,23 +52,22 @@ class LL {
     deletedLast(val) {
         let newNode = new Node(val)
         if (this.head === null) {
-            console.log("This is EMpty Linked list")
+            console.log("Thi is empty Linked List")
             return
         }
-        this.size--
-            let temp = this.head
-        while (temp.next.next !== null) {
+        let temp = this.head
+        while (temp.next.next != null) {
             temp = temp.next
         }
         temp.next = null
     }
+
     inserAtPos(val, pos) {
         this.size++
             let newNode = new Node(val)
         if (pos < 0 && pos > this.size + 1) {
-            console.log("not Posible")
+            console.log("Not Posible")
             return
-
         }
         let temp = this.head
         for (let i = 1; i < pos - 1; i++) {
@@ -76,6 +75,29 @@ class LL {
         }
         newNode.next = temp.next
         temp.next = newNode
+    }
+
+    deletedPos(val, pos) {
+        if (this.head == null) {
+            console.log("This is Empty Linked List")
+            return
+        }
+        if (pos < 0 && pos > this.size + 1) {
+            console.log("not Posible")
+            return
+        }
+        if (pos == 1) {
+            this.head = this.head.next
+            this.size--
+                return
+        }
+        let temp = this.head
+        for (let i = 1; i < pos - 1; i++) {
+            let temp = temp.next
+        }
+
+        temp.next = temp.next.next
+        this.size--
 
     }
 
@@ -110,4 +132,6 @@ obj.printLL(obj.data)
 obj.deletedLast()
 obj.printLL(obj.data)
 obj.inserAtPos(49, 2)
+obj.printLL(obj.data)
+obj.deletedPos(2)
 obj.printLL(obj.data)
