c9984d3c6fa504b150f4f05b39d4d0651b0615dd
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-16 08:48:29 +0530

    cycleSort

---
+++ linkedList/cycleList.js
@@ -0,0 +1,19 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+let head = new Node(1)
+head.next = new Node(2)
+head.next.next = new Node(3)
+head.next.next.next = new Node(4)
+head.next.next.next.next = head.next
+
+
+
+
+function hascycle(head) {
+
+}
