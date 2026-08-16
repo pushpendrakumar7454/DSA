ff8c80ba34826b15b0763637f30feb30cac343b4
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-16 09:01:34 +0530

    cycle sort

--- linkedList/cycleList.js
+++ linkedList/cycleList.js
@@ -8,23 +8,22 @@ class Node {
 let head = new Node(1)
 head.next = new Node(2)
 head.next.next = new Node(3)
-head.next.next.next = new Node(4)
-head.next.next.next.next = head.next
+head.next.next = head.next
 
 
 
-
-function hascycle(head) {
+function cycleSort(head) {
     let slow = head
     let fast = head
     while (fast != null && fast.next != null) {
         slow = slow.next
         fast = fast.next.next
-
-        if (slow === fast) {
+        if (slow == fast) {
             return true
         }
+
     }
     return false
 }
-console.log(hascycle(head))
+
+console.log(cycleSort(head))
