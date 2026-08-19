9c864ffde09ef982aa91176321544204e19d79af
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-19 08:31:03 +0530

    add two number

--- LinkesList/addtwonumberList.js
+++ LinkesList/addtwonumberList.js
@@ -14,52 +14,34 @@ l2.next = new Node(4)
 l2.next.next = new Node(7)
 
 
-// let addtwonumber = function(l1, l2) {
-
-//     let dummy = new Node(0)
-//     let curr = dummy
-//     let carry = 0
-
-//     while (l1 !== null || l2 !== null || carry !== 0) {
-
-//         let val1 = l1 ? l1.val : 0
-//         let val2 = l2 ? l2.val : 0
-
-//         let sum = val1 + val2 + carry
-
-//         carry = Math.floor(sum / 10)
-
-//         curr.next = new Node(sum % 10)
-
-//         curr = curr.next
-
-//         if (l1) l1 = l1.next
-//         if (l2) l2 = l2.next
-//     }
-
-//     return dummy.next
-// }
-
-
 let addtwonumber = function(l1, l2) {
+
     let dummy = new Node(0)
     let curr = dummy
-    let curry = 0
+    let carry = 0
 
-    while (l1 != null || l2 != null || curry !== 0) {
+    while (l1 !== null || l2 !== null || carry !== 0) {
+
         let val1 = l1 ? l1.val : 0
         let val2 = l2 ? l2.val : 0
 
-        let sum = val1 + val2 + curry
-        curry = Math.floor(sum / 10)
+        let sum = val1 + val2 + carry
+
+        carry = Math.floor(sum / 10)
+
         curr.next = new Node(sum % 10)
+
         curr = curr.next
 
         if (l1) l1 = l1.next
         if (l2) l2 = l2.next
     }
+
     return dummy.next
 }
+
+
+
 
 function printList(head) {
     let curr = head
