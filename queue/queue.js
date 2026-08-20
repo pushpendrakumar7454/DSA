c42d302ee58a5e7739fcd79ac7fb4ad1c3cbe193
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-20 07:29:10 +0530

    queue

--- queue/queue.js
+++ queue/queue.js
@@ -5,58 +5,54 @@ class Node {
     }
 }
 
-
 class Queue {
     constructor() {
         this.head = null
     }
-
     enqueue(val) {
         let newNode = new Node(val)
+
         if (this.head == null) {
             this.head = newNode
             return
-
         }
         let temp = this.head
-        while (temp.next != null) {
+        while (temp.next !== null) {
             temp = temp.next
-
         }
         temp.next = newNode
-    }
 
-    printQueue() {
-        if (this.head === null) {
-            console.log("this is  empty linked list")
+    }
+    printQueue(val) {
+        if (this.head == null) {
+
+            console.log("this is empty Linked List")
             return
         }
         let temp = this.head
-
         while (temp != null) {
-            process.stdout.write(temp.data + "->")
+            process.stdout.write(temp.data + '->')
             temp = temp.next
         }
         console.log()
+
     }
-    dqueue(val) {
-        if (this.head === null) {
+
+    dequeue(val) {
+        let newNode = new Node(val)
+        if (this.head == null) {
             console.log("this is empty Linked List")
             return
         }
         this.head = this.head.next
-
     }
-
 }
 
-
 let Q = new Queue()
-
 Q.enqueue(10)
 Q.enqueue(20)
 Q.enqueue(30)
 Q.enqueue(40)
 Q.printQueue()
-Q.dqueue()
+Q.dequeue()
 Q.printQueue()
