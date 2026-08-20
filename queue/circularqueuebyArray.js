c47e0a9c84458b0681331b231960c65be7743540
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-20 08:26:03 +0530

    circular queue by Array

--- queue/circularqueuebyArray.js
+++ queue/circularqueuebyArray.js
@@ -4,31 +4,27 @@ class Queue {
         this.arr = new Array(size)
         this.front = -1
         this.rear = -1
-    }
 
+    }
     enqueue(val) {
         if ((this.rear + 1) % this.size == this.front) {
-            console.log("full queue")
+            console.log("full Queue")
             return
         }
-
         if (this.front == -1) {
             this.front = 0
         }
-
         this.rear = (this.rear + 1) % this.size
-
         this.arr[this.rear] = val
     }
+
 }
 
 let q = new Queue(5)
-
 q.enqueue(10)
 q.enqueue(20)
 q.enqueue(30)
 q.enqueue(40)
 q.enqueue(50)
-
 console.log(q.arr)
-q.enqueue(30)
+q.enqueue(10)
