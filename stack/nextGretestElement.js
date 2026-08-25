2f333d3210003d1bba2b52b418bb690baa10a19f
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-25 07:14:19 +0530

    next Greater Element

--- stack/nextGretestElement.js
+++ stack/nextGretestElement.js
@@ -1,23 +1,15 @@
 function nextGreaterElement(arr) {
-    let st = [];
-    let ans = new Array(arr.length);
-
+    let st = []
+    let ans = new Array(arr.length)
     for (let i = arr.length - 1; i >= 0; i--) {
-
-        while (st.length && st[st.length - 1] <= arr[i]) {
-            st.pop();
+        while (st.length && st[st.length - 1] < arr[i]) {
+            st.pop()
         }
-
-
-        ans[i] = st.length ? st[st.length - 1] : -1;
-
-
-        st.push(arr[i]);
+        ans[i] = st.length ? st[st.length - 1] : -1
+        st.push(arr[i])
     }
-
-    return ans;
+    return ans
 }
 
-let arr = [4, 5, 2, 10, 8];
-
-console.log(nextGreaterElement(arr));
+let arr = [1, 2, 3, 4, 5]
+console.log(nextGreaterElement(arr))
