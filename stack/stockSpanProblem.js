6c4e40f7fe4be8bc09034424e293f4082a641593
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-25 07:59:23 +0530

    stock Span Problem

--- stack/stockSpanProblem.js
+++ stack/stockSpanProblem.js
@@ -4,15 +4,15 @@ function stockSpanProblem(arr) {
     for (let i = 0; i < arr.length; i++) {
         while (st.length != 0 && arr[st[st.length - 1]] < arr[i]) {
             st.pop()
-
         }
-        let peakIndex = st[st.length - 1]
-        if (st.length == 0) ans[i] = i + 1
-        else ans[i] = i - peakIndex
+        let peakndex = st[st.length - 1]
+        if (st.length === 0) ans[i] = i + 1
+        else ans[i] = i - peakndex
         st.push(i)
-
     }
     return ans
 }
+
+
 let arr = [100, 80, 90, 120]
 console.log(stockSpanProblem(arr))
