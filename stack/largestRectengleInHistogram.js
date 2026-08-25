562eb474200ce4ac0cfdf502c902a0b051b24bbb
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-25 09:01:29 +0530

    larges tRectangle In Histogram

--- stack/largestRectengleInHistogram.js
+++ stack/largestRectengleInHistogram.js
@@ -1,18 +1,18 @@
 function largestRectangleInHistogram(arr) {
     let st = []
     let maxArea = 0
+
     for (let i = 0; i <= arr.length; i++) {
         let currentHeight = i === arr.length ? 0 : arr[i]
         while (st.length && arr[st[st.length - 1]] > currentHeight) {
             let height = arr[st.pop()]
             let width = st.length ? i - st[st.length - 1] - 1 : i
-            let area = height * width
+            const area = height * width
             maxArea = Math.max(maxArea, area)
         }
         st.push(i)
     }
     return maxArea
-
 }
 let arr = [2, 1, 5, 6, 2, 3]
 
