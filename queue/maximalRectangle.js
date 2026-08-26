32c2db35451a4ace6c93beb6aa59518e6146bebc
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-26 08:19:55 +0530

    Maximal Rectangle

--- queue/maximalRectangle.js
+++ queue/maximalRectangle.js
@@ -5,7 +5,7 @@ function maximalRectangle(matrix) {
 
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < cols; j++) {
-            if (matrix[i][j] == "1") {
+            if (matrix[i][j] === "1") {
                 heights[j]++
             } else {
                 heights[j] = 0
@@ -19,12 +19,12 @@ function maximalRectangle(matrix) {
                 let width = j - stack[stack.length - 1] - 1
                 let area = height * width
                 maxArea = Math.max(maxArea, area)
-
             }
             stack.push(j)
         }
     }
     return maxArea
+
 }
 let matrix = [
     ["1", "0", "1", "0", "0"],
