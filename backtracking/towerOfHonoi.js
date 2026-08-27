ce049626b6b08eb35d53a7bd257a9da8e4635cce
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-27 08:26:28 +0530

    Tower Of Honoi

--- backtracking/towerOfHonoi.js
+++ backtracking/towerOfHonoi.js
@@ -1,12 +1,12 @@
-let n = 3;
-towerOfHonoi(n, "src", "help", "desc")
+let n = 3
+towerOfHonoi(n, "src", "hel", "des")
 
 function towerOfHonoi(n, src, hel, des) {
     if (n == 1) {
-        console.log(`move ${n}th place from ${src} to ${des} `)
+        console.log(`move ${n}th place from ${src} to ${des}`)
         return
     }
-    towerOfHonoi(n - 1, src, des, hel);
-    console.log(`move ${n}th place  from ${src} to ${des}`)
+    towerOfHonoi(n - 1, src, des, hel)
+    console.log(`move ${n}th place from ${src} to ${des}`)
     towerOfHonoi(n - 1, hel, src, des)
 }
