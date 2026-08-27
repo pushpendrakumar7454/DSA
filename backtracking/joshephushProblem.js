463b1a4f5b341a3a94d07be642c1fd48b86cc5ec
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-08-27 08:57:13 +0530

    Josephus Problem

--- backtracking/joshephushProblem.js
+++ backtracking/joshephushProblem.js
@@ -1,17 +1,17 @@
 function solve(arr, i, k) {
-    if (arr.length === 1) return arr[0]
+    if (arr.length == 1) return arr[0]
     let remove = (i + k) % arr.length
     arr.splice(remove, 1)
     return solve(arr, remove, k)
 }
 
-function findTheWinner(n, k) {
+
+function findOfWinner(n, k) {
     let arr = []
-    for (let i = 1; i <= n; i++) {
-        arr.push(i)
-    }
+    for (let i = 1; i <= n; i++) arr.push(i)
     k = k - 1
     return solve(arr, 0, k)
 }
 
-console.log(findTheWinner(5, 2))
+let r = findOfWinner(5, 2)
+console.log(r)
