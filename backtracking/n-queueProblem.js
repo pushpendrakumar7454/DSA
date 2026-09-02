f758f6a7e59e698641fd5d34d14c832c1c8f6df0
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-02 08:21:18 +0530

    N QUeue solution

--- backtracking/n-queueProblem.js
+++ backtracking/n-queueProblem.js
@@ -10,7 +10,7 @@ function isValid(mat, i, j) {
     row = i
     let col = j
     while (row >= 0 && col >= 0) {
-        if (mat[row][col] == "Q") {
+        if (mat[row][col] === "Q") {
             return false
         }
         row--
@@ -19,7 +19,7 @@ function isValid(mat, i, j) {
     row = i
     col = j
     while (row >= 0 && col < mat[0].length) {
-        if (mat[row][col] === "Q") {
+        if (mat[row][col] == "Q") {
             return false
         }
         row--
@@ -29,23 +29,26 @@ function isValid(mat, i, j) {
 }
 
 
+
 function solve(mat, i, ans) {
     if (i === mat.length) {
         let curr = []
         for (let t = 0; t < mat.length; t++) {
             let s = ""
             for (let r = 0; r < mat[0].length; r++) {
-                if (mat[t][r] == "Q") {
-                    s += 'Q'
+                if (mat[t][r] === "Q") {
+                    s += "Q"
                 } else {
                     s += "."
                 }
+
             }
             curr.push(s)
         }
         ans.push(curr)
         return
     }
+
     for (let col = 0; col < mat[0].length; col++) {
         if (isValid(mat, i, col)) {
             mat[i][col] = "Q"
@@ -55,12 +58,10 @@ function solve(mat, i, ans) {
     }
 }
 
-
 function nQueens(n) {
     let mat = Array.from({ length: n },
         () => Array(n).fill(".")
     );
-
     let ans = [];
 
     solve(mat, 0, ans);
