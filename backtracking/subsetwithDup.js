6996783bc8f0b315b99aa20a0edb8d1141eaeccf
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-03 08:15:08 +0530

    subsetWithDUp

---
+++ backtracking/subsetwithDup.js
@@ -0,0 +1,26 @@
+function solve(ans, curr, i, nums) {
+    ans.push([...curr])
+    while (i < nums.length) {
+        curr.push(nums[i])
+
+        solve(ans, curr, i + 1, nums)
+
+        curr.pop()
+
+        // same level ke duplicate elements skip karo
+        let current = nums[i]
+
+        while (i < nums.length && nums[i] === current) {
+            i++
+        }
+    }
+}
+
+function subsetsWithDup(nums) {
+    nums.sort((a, b) => a - b)
+    let ans = []
+    solve(ans, [], 0, nums)
+    return ans
+}
+
+console.log(subsetsWithDup([1, 2, 3]))
