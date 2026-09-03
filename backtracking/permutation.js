3173ad7d3a31fc82e044366e237e08a5f875c43a
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-03 07:20:13 +0530

    permutations

--- backtracking/permutation.js
+++ backtracking/permutation.js
@@ -1,24 +1,28 @@
 function solve(ans, curr, isVis, i, nums) {
     if (i == nums.length) {
-        ans.pudh([...curr])
+        ans.push([...curr])
         return
     }
+
     for (let j = 0; j < nums.length; j++) {
         if (!isVis[j]) {
             curr.push(nums[j])
             isVis[j] = true
+
             solve(ans, curr, isVis, i + 1, nums)
+
             curr.pop()
             isVis[j] = false
-
         }
     }
 }
 
-
-
-function purmute(nums) {
+function permute(nums) {
     let ans = []
+
     solve(ans, [], [], 0, nums)
+
     return ans
 }
+
+console.log(permute([1, 2, 3]))
