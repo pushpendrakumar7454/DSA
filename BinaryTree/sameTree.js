7a46287cee26e489a4888866397600a8cbe152e4
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-06 08:35:22 +0530

    same

--- BinaryTree/sameTree.js
+++ BinaryTree/sameTree.js
@@ -7,33 +7,17 @@ class Node {
 }
 
 function isSameTree(p, q) {
-    if (p === null && q === null) {
-        return true
-    }
-
-    if (p === null || q === null) {
-        return false
-    }
-
-    if (p.val !== q.val) {
-        return false
-    }
-
-    return (
-        isSameTree(p.left, q.left) &&
-        isSameTree(p.right, q.right)
-    )
+    if (p === null && q === null) return true
+    if (p === null || q === null) return false
+    if (p.val !== q.val) return false
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 }
 
-// Tree 1
 let p = new Node(1)
 p.left = new Node(2)
 p.right = new Node(3)
 
-// Tree 2
 let q = new Node(1)
 q.left = new Node(2)
 q.right = new Node(3)
-
-// Print result
 console.log(isSameTree(p, q))
