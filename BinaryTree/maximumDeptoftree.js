bdd248fab9c0684f2fd6d3990916714ca9bec7e0
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-06 07:50:22 +0530

    m

--- BinaryTree/maximumDeptoftree.js
+++ BinaryTree/maximumDeptoftree.js
@@ -1,33 +1,33 @@
 class Node {
     constructor(val) {
-        this.val = val;
+        this.val = val
         this.left = null
         this.right = null
     }
 }
 
-
 class Tree {
     constructor() {
         this.root = null
     }
-    buildTree() {
+    buildtree() {
         this.root = new Node(1)
         this.root.left = new Node(2)
         this.root.right = new Node(3)
-        this.root.left.right = new Node(4)
-        this.root.left.left = new Node(5)
+        this.root.left.left = new Node(4)
+        this.root.left.right = new Node(5)
         this.root.left.right = new Node(6)
         this.root.right.right = new Node(7)
     }
-    maximumdeftOfTree(node) {
+    maximum(node) {
         if (node === null) return 0
-        let left = this.maximumdeftOfTree(node.left)
-        let right = this.maximumdeftOfTree(node.right)
+        let left = this.maximum(node.left)
+        let right = this.maximum(node.right)
         return 1 + Math.max(left, right)
     }
+
 }
 
 let tree = new Tree()
-tree.buildTree()
-console.log(tree.maximumdeftOfTree(tree.root))
+tree.buildtree()
+console.log(tree.maximum(tree.root))
