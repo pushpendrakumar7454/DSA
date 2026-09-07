6bf0b170cd03e973b9119a837001a644f9d6a061
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-07 08:53:32 +0530

    pathsum

---
+++ BinaryTree/pathsum.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+function hasPathSum(root, targetSum) {
+    if (root === null) return false
+
+    // Leaf node
+    if (root.left === null && root.right === null) {
+        return root.val === targetSum
+    }
+
+    const remainingSum = targetSum - root.val
+
+    return (
+        hasPathSum(root.left, remainingSum) ||
+        hasPathSum(root.right, remainingSum)
+    )
+}
+
+let root = new Node(5)
+
+root.left = new Node(4)
+root.right = new Node(8)
+
+root.left.left = new Node(11)
+
+root.left.left.left = new Node(7)
+root.left.left.right = new Node(2)
+
+root.right.left = new Node(13)
+root.right.right = new Node(4)
+
+console.log(hasPathSum(root, 22))
