f774f28e1ef76d2e217bd0d279c6aa99c01376d9
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-07 08:20:50 +0530

    s

--- BinaryTree/symmetricTree.js
+++ BinaryTree/symmetricTree.js
@@ -6,27 +6,20 @@ class Node {
     }
 }
 
-function isSymmetric(root) {
+function issmetric(root) {
     if (root === null) return true
 
-    function isMirror(left, right) {
-        if (left === null && right === null) return true
-
-        if (left === null || right === null) return false
-
+    function isMinner(left, right) {
+        if (left == null && right === null) return true
+        if (left === null || right == -null) return false
         if (left.val !== right.val) return false
-
-        return (
-            isMirror(left.left, right.right) &&
-            isMirror(left.right, right.left)
-        )
+        return isMinner(left.left, right.right) && isMinner(left.right, right.left)
     }
+    return isMinner(root.left, root.right)
 
-    return isMirror(root.left, root.right)
 }
 
 let root = new Node(1)
-
 root.left = new Node(2)
 root.right = new Node(2)
 
@@ -35,5 +28,4 @@ root.left.right = new Node(4)
 
 root.right.left = new Node(4)
 root.right.right = new Node(3)
-
-console.log(isSymmetric(root))
+console.log(issmetric(root))
