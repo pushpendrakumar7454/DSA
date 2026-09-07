aec3e430fe4518ff9abb6bd5521ffd8be8bf2199
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-07 09:15:34 +0530

    invert tree

---
+++ BinaryTree/invertBinary.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+function invertTree(root) {
+    if (root === null) return null
+
+    // left aur right ko swap karo
+    let temp = root.left
+    root.left = root.right
+    root.right = temp
+
+    // recursively left aur right ko invert karo
+    invertTree(root.left)
+    invertTree(root.right)
+
+    return root
+}
+
+let root = new Node(4)
+
+root.left = new Node(2)
+root.right = new Node(7)
+
+root.left.left = new Node(1)
+root.left.right = new Node(3)
+
+root.right.left = new Node(6)
+root.right.right = new Node(9)
+
+invertTree(root)
+
+console.log(root)
