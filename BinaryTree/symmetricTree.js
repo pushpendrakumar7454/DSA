e0d342c47682a6aef908d956ecadd5871e46cf17
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-07 08:08:09 +0530

    symetric tree

---
+++ BinaryTree/symmetricTree.js
@@ -0,0 +1,39 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+function isSymmetric(root) {
+    if (root === null) return true
+
+    function isMirror(left, right) {
+        if (left === null && right === null) return true
+
+        if (left === null || right === null) return false
+
+        if (left.val !== right.val) return false
+
+        return (
+            isMirror(left.left, right.right) &&
+            isMirror(left.right, right.left)
+        )
+    }
+
+    return isMirror(root.left, root.right)
+}
+
+let root = new Node(1)
+
+root.left = new Node(2)
+root.right = new Node(2)
+
+root.left.left = new Node(3)
+root.left.right = new Node(4)
+
+root.right.left = new Node(4)
+root.right.right = new Node(3)
+
+console.log(isSymmetric(root))
