fb0c9940e4f0f615d659aa6fcee394e8137612de
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-08 08:29:51 +0530

    f

--- BinaryTree/leftviewOfDsf.js
+++ BinaryTree/leftviewOfDsf.js
@@ -8,6 +8,7 @@ class Node {
 
 function leftView(root) {
     if (root === null) return
+
     let maxLevel = -1
 
     function dfs(root, level) {
@@ -16,10 +17,7 @@ function leftView(root) {
             console.log(root.val)
             maxLevel = level
         }
-        dfs(root.left, level + 1)
-        dfs(root.right, level + 1)
     }
-    dfs(root, 0)
 }
 
 let root = new Node(1)
