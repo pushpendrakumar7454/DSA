8c2ea0c3199e6df8ed05289a4c8c4d75a20df258
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-09 08:03:57 +0530

    p

--- BinaryTree/lowest_common_ancestor.js
+++ BinaryTree/lowest_common_ancestor.js
@@ -6,37 +6,25 @@ class Node {
     }
 }
 
-
-// LCA Function
 function solve(root, p, q) {
 
-    // Base case
     if (root === null) {
         return null
     }
-
-    // Agar current node p ya q hai
     if (root.val === p || root.val === q) {
         return root
     }
 
-    // Left subtree
     let left = solve(root.left, p, q)
-
-    // Right subtree
     let right = solve(root.right, p, q)
-
-    // Agar left mein kuch nahi mila
     if (left === null) {
         return right
     }
-
-    // Agar right mein kuch nahi mila
     if (right === null) {
         return left
     }
 
-    // Dono side p aur q mile
+
     return root
 }
 
