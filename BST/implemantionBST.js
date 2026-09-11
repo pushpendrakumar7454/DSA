1bb4b1b673e55b77a0735348fa721c699faf7899
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-11 08:48:50 +0530

    implemention of bst

---
+++ BST/implemantionBST.js
@@ -0,0 +1,58 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+
+class BST {
+    constructor() {
+        this.root = null
+    }
+
+    inserNote(root, val) {
+
+        if (root == null) {
+            root = new Node(val)
+            return root
+        }
+
+        if (root.val > val) {
+            root.left = this.inserNote(root.left, val)
+        }
+        else if (root.val < val) {
+            root.right = this.inserNote(root.right, val)
+        }
+
+        return root
+    }
+
+
+    LDR(root) {
+
+        if (root == null) return
+
+        this.LDR(root.left)
+
+        process.stdout.write(root.val + " ")
+
+        this.LDR(root.right)
+    }
+}
+
+
+let obj = new BST()
+
+obj.root = obj.inserNote(obj.root, 50)
+obj.root = obj.inserNote(obj.root, 40)
+obj.root = obj.inserNote(obj.root, 30)
+obj.root = obj.inserNote(obj.root, 45)
+obj.root = obj.inserNote(obj.root, 55)
+obj.root = obj.inserNote(obj.root, 60)
+
+
+console.log("Inorder:")
+
+obj.LDR(obj.root)
