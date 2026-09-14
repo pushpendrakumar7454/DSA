9036bf03b9a774fc052c802de1e56105f6d0b455
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-14 07:43:30 +0530

    f

---
+++ silidingWindow/maxSumSubArray.js
@@ -0,0 +1,17 @@
+function maxSubarraySum(arr, k) {
+    let maxSum = -Infinity;
+
+    for (let i = 0; i <= arr.length - k; i++) {
+        let sum = 0;
+
+        for (let j = i; j < i + k; j++) {
+            sum += arr[j];
+        }
+        maxSum = Math.max(maxSum, sum);
+    }
+    return maxSum;
+}
+const arr = [2, 1, 5, 1, 3, 2];
+const k = 3;
+
+console.log(maxSubarraySum(arr, k));
