6e36328378ea86ee5b7669d26b8eebadb8b46511
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-14 08:16:33 +0530

    max SUbArray

--- silidingWindow/maxSumSubArray.js
+++ silidingWindow/maxSumSubArray.js
@@ -19,26 +19,25 @@
 
 // console.log(maxSubArray(arr,k))
 
-function maxSubArray(arr, k) {
-    let i = 0;
-    let j = 0;
-    let sum = 0;
-    let max = 0;
-    while (j < arr.length) {
-        sum += arr[j];
-        if (j - i + 1 < k) {
-            j++;
-        } else {
-            max = Math.max(max, sum);
-            sum -= arr[i];
-            i++;
-            j++;
+//optimize approach
+
+function maxSubArray(arr,k){
+    let i=0;j=0,max=0,sum=0
+
+    while(j<arr.length){
+        sum+=arr[j]
+
+        if(j-i+1<k){
+            j++
+        }else{
+            max=Math.max(max,sum)
+            sum-=arr[i]
+            i++
+            j++
         }
     }
-
-    return max;
+    return max
 }
-
 const arr = [3, 4, 5, 6, 8, 9];
 const k = 2;
 
