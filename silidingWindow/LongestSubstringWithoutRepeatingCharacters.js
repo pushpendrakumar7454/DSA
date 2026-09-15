1e544860d68d9d5d0c62005b678e17cc51ad47ec
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-15 08:03:08 +0530

    largest SubString

--- silidingWindow/LongestSubstringWithoutRepeatingCharacters.js
+++ silidingWindow/LongestSubstringWithoutRepeatingCharacters.js
@@ -1,3 +1,17 @@
-function longestSubarray(str){
-    
+function largestSubString(str){
+    let i=0;
+    let set=new Set()
+    let maxLength=0
+
+    for(let j=0;j<str.length;j++){
+        while(set.has(str[j])){
+            set.delete(str[i])
+            i++
+        }
+        set.add(str[j])
+        maxLength=Math.max(maxLength,j-i+1)
+    }
+    return maxLength
 }
+
+console.log(largestSubString("bbbbb"))
