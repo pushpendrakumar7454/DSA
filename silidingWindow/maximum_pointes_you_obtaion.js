7242f7321d82c586d92f3f28fd8bd952fd318cd8
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-16 09:10:09 +0530

    m

--- silidingWindow/maximum_pointes_you_obtaion.js
+++ silidingWindow/maximum_pointes_you_obtaion.js
@@ -1,22 +1,22 @@
-function maximumPointsByObtainingCards(arr, k) {
-    let leftSum = 0
+function maximumPointsByObtainingCards(arr,k){
+    let max=0,rightSum=0,leftSum=0;
 
-    for (let i = 0; i < k; i++) {
-        leftSum += arr[i]
+
+
+    for(let i=0;i<k;i++){
+        leftSum+=arr[i]
     }
 
-    let max = leftSum
-    let rightSum = 0
-    let j = arr.length - 1
+    max=leftSum
+    let j=arr.length-1
 
-    for (let i = k - 1; i >= 0; i--) {
-        leftSum -= arr[i]
-        rightSum += arr[j]
+    for(let i=k-1;i>=0;i--){
+        leftSum-=arr[i]
+        rightSum+=arr[j]
         j--
 
-        max = Math.max(max, leftSum + rightSum)
+        max=Math.max(max,leftSum+rightSum)
     }
-
     return max
 }
 
