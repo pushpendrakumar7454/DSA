fe9b8f41c56d46d504e088f3784d379c7577192a
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2026-09-16 10:12:15 +0530

    min Window

---
+++ silidingWindow/minWindow.js
@@ -0,0 +1,50 @@
+function minWindow(s, t) {
+    let count = {}
+
+    for (let char of t) {
+        count[char] = (count[char] || 0) + 1
+    }
+
+    let left = 0
+    let required = t.length
+    let minLength = Infinity
+    let start = 0
+
+    for (let right = 0; right < s.length; right++) {
+
+        if (count[s[right]] > 0) {
+            required--
+        }
+
+        count[s[right]] = (count[s[right]] || 0) - 1
+
+        while (required === 0) {
+
+            if (right - left + 1 < minLength) {
+                minLength = right - left + 1
+                start = left
+            }
+
+            count[s[left]]++
+
+            if (count[s[left]] > 0) {
+                required++
+            }
+
+            left++
+        }
+    }
+
+    if (minLength === Infinity) {
+        return ""
+    }
+
+    return s.substring(start, start + minLength)
+}
+
+
+let s = "ADOBECODEBANC"
+let t = "ABC"
+
+console.log("Minimum Window:", minWindow(s, t))
+
